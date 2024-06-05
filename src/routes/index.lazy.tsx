365a0e66fa6c1fa2f95d1853f055c55b398161a2
Hanka8 <hanka.maruskevicova@gmail.com>
2024-06-05 11:22:37 +0200

    intro

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,4 +1,5 @@
 import { createLazyFileRoute, Link } from "@tanstack/react-router";
+import "../index.css";
 
 export const Route = createLazyFileRoute("/")({
   component: Index,
@@ -6,14 +7,32 @@ export const Route = createLazyFileRoute("/")({
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-      <Link to="/project" className="text-blue-500">
-        Go to Project
-      </Link>
-      <Link to="/logos" className="text-blue-500 ml-2">
-        Go to Logos
-      </Link>
-    </div>
+    <main>
+      <section id="header" className="header">
+        <nav className="flex-center">
+          <picture className="header__picture">
+            <img className="header__img" src="assets/logo.svg" alt="logo" />
+          </picture>
+          <ul className="header__ul flex-center">
+            <li>
+              <a href="#projects">projects</a>
+            </li>
+            <li>
+              <a href="#about">about</a>
+            </li>
+          </ul>
+        </nav>
+        <h1 className="header__h1">Olga Kšírová</h1>
+      </section>
+      <section id="projects" className="projects">
+        <Link to="/project" className="text-blue-500">
+          Go to Project
+        </Link>
+        <Link to="/logos" className="text-blue-500 ml-2">
+          Go to Logos
+        </Link>
+      </section>
+      <section id="about" className="about"></section>
+    </main>
   );
 }
